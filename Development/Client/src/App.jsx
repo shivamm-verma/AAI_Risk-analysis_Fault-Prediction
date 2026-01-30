e0008c18c229d0783513f41c31edc7edf33990fd
Shivam Verma <shivam.verma256@outlook.com>
2026-01-31 01:29:15 +0530

    Refactored code & created basic UI structure

--- Development/Client/src/App.jsx
+++ Development/Client/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Tools from './pages/Tools'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/tools" element={<Tools />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
